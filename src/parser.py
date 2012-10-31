eed434a604cf7d915c5ce5302f56fd0ae6cbbe6b
Nicuvëo <crucuny@gmail.com>
2012-10-31 03:14:22 +0100

    Improved parser a bit.

--- src/parser.py
+++ src/parser.py
@@ -6,6 +6,5 @@
 
 def parse(data):
     lines = data.replace(";", "\n").strip("\n").split("\n")
-    for line_argv in [line.strip().split(" ") for line in lines]:
-        if line_argv:
-            yield line_argv
+    for line_argv in [line.strip().split(" ") for line in lines if line]:
+        yield line_argv
